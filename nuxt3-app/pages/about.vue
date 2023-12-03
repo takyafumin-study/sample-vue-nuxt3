f6f8f1ca7cbdb10575d4acb2d9dc2db2c36eeb48
takyafumin <takyafumin@gmail.com>
2023-12-03 17:58:52 +0900

    pages追加

--- nuxt3-app/pages/about.vue
+++ nuxt3-app/pages/about.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    <NuxtPage />
+    <h1>About Page</h1>
   </div>
 </template>
---
+++ nuxt3-app/pages/index.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div>
+    <h1>Main Page</h1>
+    <a href="/about">about</a>
+  </div>
+</template>
+
+<style scoped></style>
